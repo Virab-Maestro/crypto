633aaf0e2d23bfb9749c4ff92bfb5479c2386b12
Virab-Maestro <ed72akk@gmail.com>
2024-02-09 20:07:12 +0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,20 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          <div className="relative">
-            <div className="max-w-full xl:max-w-7xl mx-auto px-3 sm:px-5">
-              <Navbar/>
-              {children}
-              <Footer/>
-            </div>
-              <FramerTest classes="hidden sm:block min-[1921px]:hidden absolute top-[1%] lg:top-[1%] xl:top-[2%] 2xl:top-[0.7%] -right-[1%] 2xl:right-[0%] -z-[20]" position="left">
-                    <div className="relative">
-                      <img className="w-[400px] md:w-[500px] lg:w-[650px] 2xl:w-[870px] " src="/images/home_img.png" alt="home_img" />
-                      <div className="absolute top-[47%] right-[48%] xl:right-[55%] text-base xl:text-2xl text-center font-bold bg-[#07003E] w-[115px] h-[115px] 
-                      lg:w-[135px] lg:h-[135px] lg:text-xl xl:w-[175px] xl:h-[175px] rounded-full py-6 xl:py-10 px-4 border border-[#DF1CFF] ">Cyber Samurai #177</div>
-                    </div>
-              </FramerTest>
-          </div>
+        {children}
       </body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // output: "export", // new
+    // images: { // new
+    //   unoptimized: true,
+    // },
+    // basePath: "/crypto/", // new
     reactStrictMode: false,
 };
 
