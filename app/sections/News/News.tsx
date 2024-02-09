1003f6bdc148084201355335d58bfec7e03a1037
Virab-Maestro <ed72akk@gmail.com>
2024-02-09 16:50:48 +0300

    update: news section for mobile screns

--- app/sections/News/News.tsx
+++ app/sections/News/News.tsx
@@ -54,30 +54,29 @@ export default function News() {
             <p className="text-sm sm:text-lg 2xl:text-xl text-center max-w-96 sm:max-w-[530px] mb-10 md:mb-12 ">Set a secondary sales fee and add social links, a description, profile and banner images, and a description.</p>
           </div>
 
-          <div className="lg:hidden">
-            <Slider {...settings}>
-              <div className="max-w-[300px] sm:max-w-[370px]">
+          <div className="flex flex-col items-center gap-y-10 lg:hidden">
+            {/* <Slider {...settings}> */}
+              <a href="#" className="max-w-[300px] sm:max-w-[370px]">
                 <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4" src={news_1} alt="news-image"/>
-                <p className="text-sm mb-1">April 18, 2022</p>
-                <p className="text-md font-medium">Few Reasons to Sell NFTs on Nico</p>
-              </div>
-              <div className="max-w-[300px] sm:max-w-[370px]">
+                <p className="text-lg mb-1 opacity-55">April 18, 2022</p>
+                <p className="text-2xl font-medium">Few Reasons to Sell NFTs on Nico</p>
+              </a>
+              <a href="#" className="max-w-[300px] sm:max-w-[370px]">
                 <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4" src={news_2} alt="news-image"/>
-                <p className="text-sm mb-1">April 18, 2022</p>
-                <p className="text-md font-medium">Few Reasons to Sell NFTs on Nico</p>
-              </div>
-              <div className="max-w-[300px] sm:max-w-[370px]">
+                <p className="text-lg mb-1 opacity-55">April 18, 2022</p>
+                <p className="text-2xl font-medium">Few Reasons to Sell NFTs on Nico</p>
+              </a>
+              <a href="#" className="max-w-[300px] sm:max-w-[370px]">
                 <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4" src={news_1} alt="news-image"/>
-                <p className="text-sm mb-1">April 18, 2022</p>
-                <p className="text-md font-medium">Few Reasons to Sell NFTs on Nico</p>
-              </div>
-            </Slider>
+                <p className="text-lg mb-1 opacity-55">April 18, 2022</p>
+                <p className="text-2xl font-medium">Few Reasons to Sell NFTs on Nico</p>
+              </a>
+            {/* </Slider> */}
           </div>
 
           {/* news block after 1024px-screen */}
           <div className="relative">
             <div className="hidden w-[138%] lg:flex gap-5 xl:gap-10 justify-center items-start">
-
                 <div className="flex flex-1 flex-col items-start justify-start max-w-[420px] xl:max-w-[450px]" ref={newsInfo}>
                   <Image className="mb-6" src={news_icon} alt="news-icon"/>
                   <h2 className="text-2xl sm:text-3xl xl:text-4xl text-left font-semibold capitalize mb-7"><span className="font-normal text-[#1EE0E1] italic">Trending</span> Crypto Market News</h2>
@@ -88,21 +87,21 @@ export default function News() {
                   </div>
                 </div>
 
-                  <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
-                    <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_1} alt="news-image"/>
-                    <p className="text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
-                    <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
-                  </a>
-                  <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
-                    <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_2} alt="news-image"/>
-                    <p className="text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
-                    <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
-                  </a>
-                  <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
-                    <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_1} alt="news-image"/>
-                    <p className="text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
-                    <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
-                  </a>
+                <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
+                  <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_1} alt="news-image"/>
+                  <p className="opacity-55 text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
+                  <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
+                </a>
+                <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
+                  <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_2} alt="news-image"/>
+                  <p className="opacity-55 text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
+                  <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
+                </a>
+                <a href="#" className="news-item max-w-[300px] sm:max-w-[370px] lg:max-w-[310px] 2xl:max-w-[373px] flex-1 translate-x-24">
+                  <Image className="rounded-2xl object-cover max-w-full h-[250px] mb-4 2xl:mb-6" src={news_1} alt="news-image"/>
+                  <p className="opacity-55 text-sm lg:text-lg 2xl:text-xl mb-1 lg:mb-3 2xl:mb-5">April 18, 2022</p>
+                  <p className="text-md lg:text-2xl 2xl:text-3xl font-medium lg:font-bold">Few Reasons to Sell NFTs on Nico</p>
+                </a>
             </div>
 
             <div ref={newsBtn} className="collapse opacity-0 transition-all absolute top-[100%] left-1/2 -translate-x-1/2 mt-6 lg:mt-12 text-center">
@@ -111,7 +110,7 @@ export default function News() {
           </div>
           {/* end */}
 
-          <div className="mt-6 lg:mt-12 text-center lg:hidden">
+          <div className="mt-8 lg:mt-12 text-center lg:hidden">
             <Button1 title="Explore NFTs"></Button1>
           </div>
         </div>
